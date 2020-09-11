508a2a398e09dccbc06cb6f3eccdcb94a8b442c8
kzsc5464 <kzsc5464@naver.com>
2020-09-11 23:18:39 +0900

    익이전용 ui

---
+++ iksisback.py
@@ -0,0 +1,11 @@
+#익이전용 UI 프로젝트 건드리지마시오
+
+from tkinter import *
+
+main = Tk()
+#화면 제목 작업표시줄
+main.title("익이전용")
+#화면크기
+main.geometry("640*480")
+#화면크기 고정 앞에부터 x(너비),y(높이)
+main.resizable(False,False)
