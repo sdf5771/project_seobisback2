76f4753cecc14bbf3380614f12aed7cda0b7fecb
choose_your_ <66990091+sdf5771@users.noreply.github.com>
2020-09-12 06:40:59 +0900

    슬라이싱까지 공부한것

---
+++ practice2.py
@@ -0,0 +1,27 @@
+#연산자
+print("ㅡㅡㅡㅡ 연산자 ㅡㅡㅡㅡ")
+print(1 + 1) #2
+print(3-2) #1
+print(5*2) #10
+print(6/3) #2
+
+print(2**3) # 2^3 = 8
+print(5%3) # 나머지 구하기 2
+print(10%3) #1
+print(5//3) #1
+print(10//3) #3
+
+print(10 > 3) # 10보다 3이 더 크다True
+print(4 >= 7) # 4는 7보다 크거나 같다False
+print(10 < 3) # 10은 3보다 작다False
+print(5 <= 5) # 5는 5보다 크거나 같다True
+
+print(3 == 3) # 같다 True
+print(4 == 2) # False
+print(3 + 4 == 7) # True
+
+print(1 != 3) #같지 않다 True
+print(not(1 != 3)) # False
+
+print((3 > 0) and (3 < 5)) # True
+print((3 > 0) & (3 < 5)) # True
