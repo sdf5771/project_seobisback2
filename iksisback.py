6819a06ef5e768fd088a11859aeca1e59e09fdfe
kzsc5464 <kzsc5464@naver.com>
2020-09-12 22:45:31 +0900

    기본 툴

--- iksisback.py
+++ iksisback.py
@@ -1,11 +1,11 @@
+from tkinter import*
 #익이전용 UI 프로젝트 건드리지마시오
-
-from tkinter import *
-
 main = Tk()
+#화면이 닫치지않게하는
+main.mainloop()
 #화면 제목 작업표시줄
 main.title("익이전용")
 #화면크기
-main.geometry("640*480")
+main.geometry("1280*640")
 #화면크기 고정 앞에부터 x(너비),y(높이)
 main.resizable(False,False)
