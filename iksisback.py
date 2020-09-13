836440870b3d227228121da95161ee09ef993e03
kzsc5464 <kzsc5464@naver.com>
2020-09-14 00:39:26 +0900

    수정

--- iksisback.py
+++ iksisback.py
@@ -2,7 +2,6 @@ import tkinter
 import os
 from tkinter import filedialog
 from tkinter import Button
-from PIL import ImageTk
 #익이전용 UI 프로젝트 건드리지마시오
 main = tkinter.Tk()
 #화면 제목 작업표시줄
@@ -14,20 +13,20 @@ main.resizable(False,False)
 
 #파일추가
 
-# def add_file():
-#         files = filedialog.askopenfilenames(title="이미지 파일을 선택하세요",\
-#         filetypes=(("PNG 파일","*.png"),("모든 파일", "*.*")))
-#         print(files)
-#         faceimage = tkinter.PhotoImage(file = files)
+def add_file():
+        files = filedialog.askopenfilenames(title="이미지 파일을 선택하세요",\
+        filetypes=(("PNG 파일","*.png"),("모든 파일", "*.*")))
+        print(files)
+        # faceimage = tkinter.PhotoImage(file = files)
         
 
-faceimage=tkinter.PhotoImage(file = "test1.png")
-img = ImageTk.PhotoImage(Image.open(faceimage))
+# faceimage=tkinter.PhotoImage(file = "test1.png")
+
 # facelabel = tkinter.Label(main, image = faceimage)
 # facelabel.pack()
 
-# add_file_btn = Button(main,text="사진찾기",command=add_file)
-# add_file_btn.pack()
+add_file_btn = Button(main,text="사진찾기",command=add_file)
+add_file_btn.pack()
 
 
 
