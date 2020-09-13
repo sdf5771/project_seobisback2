8ead90106cc111ac819ed9e992cbb33d8dd30f6c
kzsc5464 <kzsc5464@naver.com>
2020-09-14 00:49:52 +0900

    다시시작

--- iksisback.py
+++ iksisback.py
@@ -1,9 +1,9 @@
-import tkinter
-import os
+
 from tkinter import filedialog
 from tkinter import Button
-#익이전용 UI 프로젝트 건드리지마시오
-main = tkinter.Tk()
+from tkinter import *
+
+main = Tk()
 #화면 제목 작업표시줄
 main.title("FLCL")
 #화면크기
@@ -17,14 +17,7 @@ def add_file():
         files = filedialog.askopenfilenames(title="이미지 파일을 선택하세요",\
         filetypes=(("PNG 파일","*.png"),("모든 파일", "*.*")))
         print(files)
-        # faceimage = tkinter.PhotoImage(file = files)
-        
-
-# faceimage=tkinter.PhotoImage(file = "test1.png")
-
-# facelabel = tkinter.Label(main, image = faceimage)
-# facelabel.pack()
-
+       
 add_file_btn = Button(main,text="사진찾기",command=add_file)
 add_file_btn.pack()
 
--- index.py
+++ index.py
@@ -1,4 +1,4 @@
-print("vscode github 연동입니다.")
-print("seobisback 연동입니다.")
-print("vscode github 테스트 완료입니다.")
-print("집컴퓨터에서 연동합니다.")
+import tkinter as tk
+
+main = tk.Tk()
+main.geometry("640x800")
