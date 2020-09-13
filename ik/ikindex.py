07d9736682a732451e70b16aa446b0b1cd91dbef
kzsc5464 <kzsc5464@naver.com>
2020-09-14 01:01:39 +0900

    수정

---
+++ ik/ikindex.py
@@ -0,0 +1,8 @@
+import tkinter as tk
+from tkinter import Button
+main = tk.Tk()
+main.geometry("640x840")
+main.mainloop
+btn1 = Button(main)
+btn1.pack()
+print("Hello")
--- index.py
+++ index.py
@@ -1,4 +1,16 @@
 import tkinter as tk
+from tkinter import Button
+from tkinter import dialog
 
 main = tk.Tk()
 main.geometry("640x800")
+main.title("FLFC")
+main.mainloop
+
+def add_file():
+        files = tk.filedialog.askopenfilenames(title="이미지 파일을 선택하세요",\
+        filetypes=(("PNG 파일","*.png"),("모든 파일", "*.*")))
+        print(files)
+       
+add_file_btn = Button(main,text="사진찾기",command=add_file)
+add_file_btn.pack()
