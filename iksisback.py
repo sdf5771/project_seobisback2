5f85c7e2f05672adc1e1456fc196e6f720bf66ad
kzsc5464 <kzsc5464@naver.com>
2020-09-15 00:37:51 +0900

    img 파일가능

--- iksisback.py
+++ iksisback.py
@@ -1,10 +1,12 @@
 
+import os
+import tkinter as tk
 from tkinter import filedialog
 from tkinter import Button
 from tkinter import Label
-from tkinter import *
+from PIL import ImageTk, Image
 
-main = Tk()
+main = tk.Tk()
 #화면 제목 작업표시줄
 main.title("FLCL")
 #화면크기
@@ -13,18 +15,23 @@ main.geometry("640x480")
 main.resizable(False,False)
 
 #파일추가
-
 def add_file():
         files = filedialog.askopenfilenames(title="이미지 파일을 선택하세요",\
         filetypes=(("PNG 파일","*.png"),("모든 파일", "*.*")))
         add_path = Label(main,text=files)
         add_path.pack()
 
-       
+
+
+image_test=Image.open("test1.jpeg")
+img = ImageTk.PhotoImage(image_test)
+image_Label1=Label(main,image=img)
+image_Label1.pack()
 add_file_btn = Button(main,text="사진찾기",command=add_file)
 add_file_btn.pack()
 add_path = Label(main,text="유후")
 add_path.pack()
+
 
 
 
